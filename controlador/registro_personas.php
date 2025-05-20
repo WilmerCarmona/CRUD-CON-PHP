58bc53279aa3298d1a21398b00f26641f2781dd7
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-05-19 20:59:43 -0500

    update

--- controlador/registro_personas.php
+++ controlador/registro_personas.php
@@ -1,26 +1,28 @@
 <?php
-    if(!empty($_POST["btnregistrar"])) {
-        if (!empty($_POST["nombre"]) and 
-            !empty($_POST["apellidos"]) and
-            !empty($_POST["identif"]) and 
-            !empty($_POST["fecha"]) and
-            !empty($_POST["correo"])){
-          
-            $nombre=$_POST["nombre"];  
-            $apellidos=$_POST["apellidos"];
-            $identif=$_POST["identif"];
-            $fecha=$_POST["fecha"];
-            $email=$_POST["correo"];
+if (!empty($_POST["btnregistrar"])) {
+    if (
+        !empty($_POST["nombre"]) and
+        !empty($_POST["apellidos"]) and
+        !empty($_POST["identif"]) and
+        !empty($_POST["fecha"]) and
+        !empty($_POST["correo"])) {
+        
+        $nombre = $_POST["nombre"];
+        $apellidos = $_POST["apellidos"];
+        $identif = $_POST["identif"];
+        $fecha = $_POST["fecha"];
+        $email = $_POST["correo"];
 
-            $sql=$base->query("insert into personas(nom_per,ape_per,iden_per,fecha_per,email_per)values('$nombre','$apellidos','$identif','$fecha','$email') ");
-                if ($sql==1) {
-                 echo '<div class="alert alert-success">La persona se ha registrado correctamente</div>';
-                } else {
-                    echo '<div class="alert alert-darger">Error ala registrar esta persona</div>';
-                }
-                
-        }else{
-            echo '<div class="alert alert-darger">Alguno de los campos está vacío</div' ;
+        
+        $sql = $base->query("insert into personas (nom_per, ape_per, iden_per, fecha_per, email_per)values('$nombre','$apellidos','$identif','$fecha','$email')");
+
+        if ($sql) {
+            echo '<div class="alert alert-success">La persona se ha registrado correctamente.</div>';
+        } else {
+            echo '<div class="alert alert-danger">Error al registrar esta persona.</div>';
         }
+    } else {
+        echo '<div class="alert alert-danger">Alguno de los campos está vacío.</div>';
     }
+}
 ?>
